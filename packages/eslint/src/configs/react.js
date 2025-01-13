488ede53d8b692ec945f659b2192e4e8deaa38e5
Mny <email@email.com>
2025-01-13 19:10:46 +0300

    create simple react rules

--- packages/eslint/src/configs/react.js
+++ packages/eslint/src/configs/react.js
@@ -1,15 +1,31 @@
-import reactESLint from 'eslint-plugin-react';
-
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import reactPlugin from 'eslint-plugin-react';
+import tsParser from "@typescript-eslint/parser";
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
 
 
 export const react = () => {
     return {
-        files: ["src/**/*.js", "src/**/*.ts", "src/**/*.tsx","src/**/*.jsx",],
+        files: ["src/**/*.{jsx,tsx}"],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+                ecmaVersion: "latest",
+                sourceType: "module",
+            },
+        },
         plugins: {
-            react: reactESLint
+            'react': reactPlugin,
+            'react-hooks': reactHooksPlugin,
+            '@typescript-eslint': typescriptEslintPlugin,
         },
         rules: {
-            ...reactESLint.configs.recommended.rules
+            ...reactPlugin.configs.recommended.rules,
+            ...reactHooksPlugin.configs.recommended.rules,
+            ...typescriptEslintPlugin.configs.recommended.rules,
         },
     }
 }
